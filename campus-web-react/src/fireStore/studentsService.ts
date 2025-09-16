da964c0d8cfb3ab518e95d85c30654b790a0dd9f
YANIV COHEN <yaniv1499@gmail.com>
2025-09-16 18:41:41 +0300

    fix: firestore student converter

--- campus-web-react/src/fireStore/studentsService.ts
+++ campus-web-react/src/fireStore/studentsService.ts
@@ -1,27 +1,39 @@
 import { Student } from "../types/Student";
 import { firestore } from "./config";
-import { collection, addDoc,getDoc, getDocs,setDoc,doc,deleteDoc,updateDoc, QueryDocumentSnapshot } from "firebase/firestore";
-const studentConverter = {
-    toFirestore: (student: Student) => student,
-    fromFirestore: (snapshot: QueryDocumentSnapshot) => snapshot.data() as Student
-};
-const studentsCollection = collection(firestore, "students")
+import { collection, addDoc, getDoc, getDocs, setDoc, doc, deleteDoc, updateDoc, QueryDocumentSnapshot, DocumentData } from "firebase/firestore";
 
-export async function addStudent(student: Student) {
-    await addDoc(studentsCollection, student).withConverter(studentConverter);
+const studentConverter = {
+    toFirestore: (student: Student): DocumentData => student,
+    fromFirestore: (snapshot: QueryDocumentSnapshot): Student => snapshot.data() as Student
+};
+
+const studentsCollection = collection(firestore, "students").withConverter(studentConverter);
+
+export async function addStudent(student: Student): Promise<void> {
+    await addDoc(studentsCollection, student);
 }
+
 export async function listStudents(): Promise<Student[]> {
-    const querySnapshot = await getDocs(studentsCollection).withConverter(studentConverter);
-    return querySnapshot.docs.map((doc) => doc.data() as Student);
+    const querySnapshot = await getDocs(studentsCollection);
+    return querySnapshot.docs.map((doc) => doc.data());
 }
 
 export async function getStudentById(id: string): Promise<Student | null> {
-    const docSnapshot = await getDoc(doc(studentsCollection, id));
-    return docSnapshot.data() as Student | null;
+    const docRef = doc(studentsCollection, id);
+    const docSnapshot = await getDoc(docRef);
+    
+    if (docSnapshot.exists()) {
+        return docSnapshot.data();
+    }
+    return null;
 }
-export async function updateStudent(student: Student) {
-    await setDoc(doc(studentsCollection, student)).withConverter(studentConverter);
+
+export async function updateStudent(student: Student): Promise<void> {
+    const docRef = doc(studentsCollection, student.id);
+    await setDoc(docRef, student);
 }
-export async function deleteStudent(id: string) {
-    await deleteDoc(doc(studentsCollection, id)).withConverter(studentConverter);
+
+export async function deleteStudent(id: string): Promise<void> {
+    const docRef = doc(studentsCollection, id);
+    await deleteDoc(docRef);
 }
